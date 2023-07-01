eb7fdf03e37251e90bc6fbade8990a6f13b4be57
Israel Kilday <israel.kilday@yahoo.com.br>
2023-07-01 16:03:20 -0300

    adding files

---
+++ src/components/index.tsx
@@ -0,0 +1,15 @@
+import './style.css';
+
+type CardProps = {
+    name: string,
+    time: string
+}
+
+export function Card(props: CardProps) {
+    return (
+        <div className='card'>
+            <strong>{props.name}</strong>
+            <small>{props.time}</small>
+        </div>
+    )
+}
